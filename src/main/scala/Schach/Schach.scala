ead9089ecc36bf64640ebbdcd3cffb8a21c8ea29
Schmidt_jan <ja163sch@htwg-konstanz.de>
2020-11-08 20:46:41 +0100

    UI included

--- src/main/scala/Schach/Schach.scala
+++ src/main/scala/Schach/Schach.scala
@@ -3,8 +3,8 @@ package Schach
 object Schach {
 
   def createGameField(): String = {
-    val aToH = List("a", "b", "c", "d", "e", "f", "g", "h" )
-    val numbers = List("8", "7", "6", "5", "4", "3", "2", "1" )
+    val aToH = List("a", "b", "c", "d", "e", "f", "g", "h")
+    val numbers = List("8", "7", "6", "5", "4", "3", "2", "1")
     val cellSide = " │   │   │   │   │   │   │   │   │"
     val cellMiddle = "├───┼───┼───┼───┼───┼───┼───┼───┤"
     val cellBottom = "  └───┴───┴───┴───┴───┴───┴───┴───┘"
@@ -31,9 +31,38 @@ object Schach {
     field.toString()
   }
 
+  def readInput(line: String): Array[Int] = {
+    val fromX = getPoint(line.charAt(0))
+    val fromY = getPoint(line.charAt(1))
+    val toX = getPoint(line.charAt(3))
+    val toY = getPoint(line.charAt(4))
+    Array(fromX, fromY, toX, toY)
+  }
+
+  def getPoint(input: Char): Int = {
+    input match {
+      case 'A' => 0
+      case 'B' => 1
+      case 'C' => 2
+      case 'D' => 3
+      case 'E' => 4
+      case 'F' => 5
+      case 'G' => 6
+      case 'H' => 7
+      case '1' => 0
+      case '2' => 1
+      case '3' => 2
+      case '4' => 3
+      case '5' => 4
+      case '6' => 5
+      case '7' => 6
+      case '8' => 7
+      case _ => -1
+    }
+  }
 
   def main(args: Array[String]) {
-/*
+    /*
     println("Welcome to Chess")
     println("What is your name?")
     val name = scala.io.StdIn.readLine()
@@ -53,5 +82,22 @@ object Schach {
     gameField.moveTo(1, 1, 1, 3)
     gameField.moveTo(0, 0, 0, 5)
     println(gameField.toString());
+
+    var abbruch = false
+    while (abbruch == false) {
+      val line = scala.io.StdIn.readLine()
+
+      if (line.equals("exit")) {
+        abbruch = true
+      } else {
+        if (line.matches("([A-H][1-8]\\s[A-H][1-8])")) {
+          val ret = readInput(line)
+          gameField.moveTo(ret(0), ret(1), ret(2), ret(3))
+          println(gameField.toString)
+        } else {
+          println("Wrong input")
+        }
+      }
+    }
   }
 }
